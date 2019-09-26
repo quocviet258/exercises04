7b4e66282cd785a45b2a3263e1dbdcc87d28661a
Bui Quoc Viet <caubezon258@gmail.com>
2019-09-26 15:29:20 +0700

    bond1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" Hello World");
+
+        }
+        public static bool IsOdd(int n)
+
+        {
+            bool dk = false;
+            if(n % 2 ! = 0)
+            {
+                dk = true;
+            return true;
+       }
+
+        public void bool IsEven(int n)
+        {
+             bool dk = false;
+            if(n % 2 ! = 0)
+            {
+                dk = true;
+            return true;
         }
     }
 }
---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1:
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIsOdd()
+
+        {
+            bool result3 = Program.IsOdd(3);
+            Assert.AreEqual(true, result3);
+
+
+            bool result4 = Program.IsOdd(4);
+            Assert.AreEqual(false, result4);
+        }
+        
+        public void TestIsEven()
+        {
+            bool result4 =Program.IsEven(4);
+            Assert.AreEqual(true, result4);
+
+            bool result3 = Program.IsEven(3);
+            Assert.AreEqual(false, result3);
+
+    }
+        public void TestiIsPrime();
+    {
+    
+        bool result2 = Program.IsPrime(2);
+        Assert.
+            }
+       }
+}
